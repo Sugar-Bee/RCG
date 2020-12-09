dd5d45c5d01e4a729e47726e292b8267938613d1
Sugar-Bee <sugar.bee.git@gmail.com>
2020-12-09 12:08:17 +0530

    Created a Person folder and js file and creating our own component named Person

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Person from './Person/Person.js';
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi, How are you!</h1>
 				<p>subtitles!!!</p>
+				<Person></Person>
 			</div>
 		);
 	}
