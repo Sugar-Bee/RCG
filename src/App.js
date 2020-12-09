e6c7f08066f5b4068483841da74370ac894cc19d
Sugar-Bee <sugar.bee.git@gmail.com>
2020-12-09 13:57:10 +0530

    understanding usage of state and buttons

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person.js';
+
 class App extends Component {
+	state = {
+		persons: [
+			{ name: 'Max', age: 28 },
+			{ name: 'Manu', age: 22 },
+			{ name: 'stephanie', age: 20 }
+		]
+	};
+
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi, How are you!</h1>
 				<p>subtitles!!!</p>
-				<Person name="Max" age="28"></Person>
-				<Person name="Manu" age="22">
+				<button>Switch Name</button>
+				<Person
+					name={this.state.persons[0].name}
+					age={this.state.persons[0].age}></Person>
+				<Person
+					name={this.state.persons[1].name}
+					age={this.state.persons[1].age}>
 					My Hobbies: Racing
 				</Person>
-				<Person name="stephanie" age="56"></Person>
+				<Person
+					name={this.state.persons[2].name}
+					age={this.state.persons[2].age}></Person>
 			</div>
 		);
 	}
